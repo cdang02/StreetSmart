c268ffb9fa23781716c9b4583962670f63b4a95e
jasperzhu2002 <jasperzhu2002@gmail.com>
2025-04-12 14:17:12 -0400

    preliminary functions implemented

--- StreetSmart/app/_layout.tsx
+++ StreetSmart/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="routeSelection" options={{ title: 'Route Selection' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
 }
